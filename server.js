28b767f46956b0009b76dac0fe766505005af525
2940205300 <75941716+2940205300@users.noreply.github.com>
2021-04-19 15:10:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app=express();
 app.use(express.static(path.join(_dirname,'/public');//静态资源文件
                        
 app.listen(3000,()=>{
-console.log('localhost:3000')
+// console.log('localhost:3000')
 })                       
