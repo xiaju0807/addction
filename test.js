4ee81cb97826f034cbe4aac2af8cb1d3b266c058
2940205300 <75941716+2940205300@users.noreply.github.com>
2021-04-19 14:59:19 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const path=requer('path')
+const app=express();
+
+app.use(express.static(path.join(_dirname,'/public');//静态资源文件
+                       
+app.listen(3000,()=>{
+console.log('localhost:3000')
+})                       
